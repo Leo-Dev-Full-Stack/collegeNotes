3608cd05477df47fdc7fd30e180b043c3492f59c
Leo-Dev-Full-Stack <leo.devfullstack@gmail.com>
2024-08-20 17:30:14 -0300

    add newLines.C

--- LTP/notesC.c
+++ LTP/notesC.c
@@ -4,3 +4,4 @@ int main() {                // This is called a function. Any code inside its cu
   printf("Hello World!");   // printf() is a function used to output/print text to the screen.
   return 0;                 // return 0 ends the main() function.
 }                           // Do not forget to add the closing curly bracket } to actually end the main function.
+                            // Every C statement ends with a semicolon ;
