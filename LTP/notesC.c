93ff7fefa526a47c3fe215a739d36fa138433e86
Leo-Dev-Full-Stack <leo.devfullstack@gmail.com>
2024-08-20 17:18:12 -0300

    Hello World in C language

---
+++ LTP/notesC.c
@@ -0,0 +1,6 @@
+#include <stdio.h>          // header file library
+
+int main() {                // This is called a function. Any code inside its curly brackets {} will be executed.
+  printf("Hello World!");   // printf() is a function used to output/print text to the screen.
+  return 0;                 // return 0 ends the main() function.
+}                           // Do not forget to add the closing curly bracket } to actually end the main function.
